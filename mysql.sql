7a81467d3dceb79f83fcbb55668541cf7d260f0e
weinh <weinh@hundsun.com>
2017-10-13 17:05:32 +0800

    新增数据库脚本

---
+++ mysql.sql
@@ -0,0 +1,134 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.6.34 : Database - crm_dev
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `sys_menu` */
+
+DROP TABLE IF EXISTS `sys_menu`;
+
+CREATE TABLE `sys_menu` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `pid` bigint(20) NOT NULL COMMENT '父菜单ID',
+  `name` varchar(64) NOT NULL COMMENT '名称',
+  `url` varchar(256) DEFAULT NULL COMMENT '链接地址',
+  `type` varchar(16) NOT NULL COMMENT '菜单类型',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_menu` */
+
+LOCK TABLES `sys_menu` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_org` */
+
+DROP TABLE IF EXISTS `sys_org`;
+
+CREATE TABLE `sys_org` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(64) NOT NULL COMMENT '名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_org` */
+
+LOCK TABLES `sys_org` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_org_user` */
+
+DROP TABLE IF EXISTS `sys_org_user`;
+
+CREATE TABLE `sys_org_user` (
+  `org_id` bigint(20) unsigned NOT NULL COMMENT '组织ID',
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`org_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_org_user` */
+
+LOCK TABLES `sys_org_user` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(64) NOT NULL COMMENT '名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_role` */
+
+LOCK TABLES `sys_role` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_role_menu` */
+
+DROP TABLE IF EXISTS `sys_role_menu`;
+
+CREATE TABLE `sys_role_menu` (
+  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
+  `menu_id` bigint(20) unsigned NOT NULL COMMENT '菜单ID',
+  PRIMARY KEY (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_role_menu` */
+
+LOCK TABLES `sys_role_menu` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_role_user` */
+
+DROP TABLE IF EXISTS `sys_role_user`;
+
+CREATE TABLE `sys_role_user` (
+  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`role_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_role_user` */
+
+LOCK TABLES `sys_role_user` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(64) NOT NULL COMMENT '用户名',
+  `password` varchar(64) NOT NULL COMMENT '密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_user` */
+
+LOCK TABLES `sys_user` WRITE;
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
