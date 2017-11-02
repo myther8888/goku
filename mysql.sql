a1db02efe11a9d436922f893f2d1bc73d83fd515
weinh <weinh@hundsun.com>
2017-11-02 17:15:48 +0800

    菜单

--- mysql.sql
+++ mysql.sql
@@ -22,12 +22,33 @@ CREATE TABLE `sys_menu` (
   `name` varchar(64) NOT NULL COMMENT '名称',
   `url` varchar(256) DEFAULT NULL COMMENT '链接地址',
   `type` varchar(16) NOT NULL COMMENT '菜单类型',
+  `action` varchar(16) DEFAULT NULL COMMENT '操作类型',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `sys_menu` */
 
 LOCK TABLES `sys_menu` WRITE;
+
+insert  into `sys_menu`(`id`,`pid`,`name`,`url`,`type`,`action`) values (1,-1,'系统管理',NULL,'1',NULL),(2,1,'用户管理','/system/manage/user','2',NULL),(3,2,'用户新增','/system/users/user','3','create'),(4,2,'用户修改','/system/users/user/*','3','update'),(5,2,'用户查询','/system/users','3','read'),(6,2,'用户删除','/system/users/user/*','3','delete'),(7,1,'角色管理','/system/manage/role','2',NULL),(8,7,'角色新增','/system/roles/role','3','create'),(9,7,'角色修改','/system/roles/role/*','3','update'),(10,7,'角色查询','/system/roles','3','read'),(11,7,'角色删除','/system/roles/role/*','3','delete');
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_menu_role` */
+
+DROP TABLE IF EXISTS `sys_menu_role`;
+
+CREATE TABLE `sys_menu_role` (
+  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
+  `menu_id` bigint(20) unsigned NOT NULL COMMENT '菜单ID',
+  PRIMARY KEY (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_menu_role` */
+
+LOCK TABLES `sys_menu_role` WRITE;
+
+insert  into `sys_menu_role`(`role_id`,`menu_id`) values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),(1,11);
 
 UNLOCK TABLES;
 
@@ -71,43 +92,13 @@ CREATE TABLE `sys_role` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(64) NOT NULL COMMENT '名称',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `sys_role` */
 
 LOCK TABLES `sys_role` WRITE;
 
-UNLOCK TABLES;
-
-/*Table structure for table `sys_role_menu` */
-
-DROP TABLE IF EXISTS `sys_role_menu`;
-
-CREATE TABLE `sys_role_menu` (
-  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
-  `menu_id` bigint(20) unsigned NOT NULL COMMENT '菜单ID',
-  PRIMARY KEY (`role_id`,`menu_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `sys_role_menu` */
-
-LOCK TABLES `sys_role_menu` WRITE;
-
-UNLOCK TABLES;
-
-/*Table structure for table `sys_role_user` */
-
-DROP TABLE IF EXISTS `sys_role_user`;
-
-CREATE TABLE `sys_role_user` (
-  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
-  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
-  PRIMARY KEY (`role_id`,`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `sys_role_user` */
-
-LOCK TABLES `sys_role_user` WRITE;
+insert  into `sys_role`(`id`,`name`) values (1,'管理员');
 
 UNLOCK TABLES;
 
@@ -120,11 +111,31 @@ CREATE TABLE `sys_user` (
   `username` varchar(64) NOT NULL COMMENT '用户名',
   `password` varchar(64) NOT NULL COMMENT '密码',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `sys_user` */
 
 LOCK TABLES `sys_user` WRITE;
+
+insert  into `sys_user`(`id`,`username`,`password`) values (1,'admin','738b6ac910c9125916567c428b7b05f0');
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_user_role` */
+
+DROP TABLE IF EXISTS `sys_user_role`;
+
+CREATE TABLE `sys_user_role` (
+  `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`role_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_user_role` */
+
+LOCK TABLES `sys_user_role` WRITE;
+
+insert  into `sys_user_role`(`role_id`,`user_id`) values (1,1);
 
 UNLOCK TABLES;
 
