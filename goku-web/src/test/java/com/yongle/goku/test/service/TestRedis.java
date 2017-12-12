ec547ccec54886dbd16fc2f60ad23e6f624d4fdc
yonglelaoren <yonglelaoren@163.com>
2017-12-12 22:36:23 +0800

    微调

--- goku-web/src/test/java/com/yongle/goku/test/service/TestRedis.java
+++ goku-web/src/test/java/com/yongle/goku/test/service/TestRedis.java
@@ -1,13 +1,10 @@
 package com.yongle.goku.test.service;
 
-import com.alibaba.fastjson.JSONObject;
-import com.yongle.goku.system.service.UserService;
 import org.junit.Test;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
 
@@ -16,15 +13,12 @@ import javax.annotation.Resource;
  */
 public class TestRedis extends JUnitServiceBase {
     @Resource
-    StringRedisTemplate stringRedisTemplate;
-
-    @Resource
-    UserService userService;
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     public void testPipeline() {
         long t1 = System.currentTimeMillis();
-        stringRedisTemplate.executePipelined(new RedisCallback<String>() {
+        redisTemplate.executePipelined(new RedisCallback<String>() {
             @Override
             public String doInRedis(RedisConnection connection) throws DataAccessException {
                 for (int i = 0; i < 100000; i++) {
@@ -37,7 +31,7 @@ public class TestRedis extends JUnitServiceBase {
 
         long t2 = System.currentTimeMillis();
         for (int i = 0; i < 100000; i++) {
-            stringRedisTemplate.opsForValue().set(String.valueOf(i), String.valueOf(i));
+            redisTemplate.opsForValue().set(String.valueOf(i), String.valueOf(i));
         }
         System.out.println("execute non Pipelined:" + (System.currentTimeMillis() - t2));
     }
@@ -46,7 +40,7 @@ public class TestRedis extends JUnitServiceBase {
     public void testSentinel() throws InterruptedException {
         for (int i = 0; i < 100000; i++) {
             try {
-                stringRedisTemplate.opsForValue().set(String.valueOf(i), String.valueOf(i));
+                redisTemplate.opsForValue().set(String.valueOf(i), String.valueOf(i));
                 log.info("正在向redis中添加：{}", i);
                 Thread.sleep(1000);
             } catch (Exception e) {
@@ -59,7 +53,7 @@ public class TestRedis extends JUnitServiceBase {
     public void testCluster() {
         for (int i = 0; i < 100000; i++) {
             try {
-                stringRedisTemplate.opsForValue().set(String.valueOf(i), String.valueOf(i));
+                redisTemplate.opsForValue().set(String.valueOf(i), String.valueOf(i));
                 log.info("正在向redis中添加：{}", i);
             } catch (Exception e) {
                 log.error(e.getMessage(), e);
